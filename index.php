db79acb7bdc7a448b548d30ba3c7e91dde95f168
Héctor Gómez <hector.agr@gmail.com>
2014-03-26 00:15:38 -0600

    First Commit
    Commit inicial con Furatto

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html lang="es_mx">
+<head>
+<meta charset="utf-8"/>
+<title>Caro2China</title>
+<link rel="stylesheet" href="css/furatto.min.css"/>
+</head>
+<body>
+<nav>
+</nav>
+<footer>
+</footer>
+</body>
+</html>
