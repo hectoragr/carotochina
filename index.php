934bb0c0a7efa23541b1f57c195efb2fc40b3798
Héctor Gómez <hector.agr@gmail.com>
2014-03-26 00:37:00 -0600

    Blank Example Furatto

--- index.php
+++ index.php
@@ -1,14 +1,61 @@
 <!DOCTYPE>
-<html lang="es_mx">
-<head>
-<meta charset="utf-8"/>
-<title>Caro2China</title>
-<link rel="stylesheet" href="css/furatto.min.css"/>
+<html lang="es_mx"><head>
+<!--includes meta tags, title and more header definitions-->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+<meta name="description" content="Web page for helping funding School trip to China">
+<meta name="keywords" content="China, Charity, Funding, Help, Trip, Study, School">
+<meta name="author" content="Héctor Gómez">
+<link rel="icon" type="image/png" href="">
+<title>
+  Caro2China
+</title>
+
+<!-- Furatto core CSS -->
+<link href="css/normalize.css" rel="stylesheet">
+<link href="css/furatto.min.css" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<style type="text/css"></style>
 </head>
-<body>
-<nav>
+<body style="">
+
+<nav class="navigation-bar ">
+   <ul class="brand-section">
+     <li class="brand-name">
+      <a href="./" class="menu-trigger" id="trigger">Caro To China</a>
+     </li>
+     <li class="menu-toggle">
+      <a href="#"></a>
+     </li>
+   </ul>
+   <ul class="pull-right">
+     <li class="divider"></li>
+     <li><a href="#">Contacto</a></li>
+     <li class="divider"></li>
+     <li><a href="#" class="button danger three-d">Apoyo</a></li>
+   </ul>
 </nav>
-<footer>
-</footer>
+
+
+<div class="row">
+  <h1 class="text-center">Caro to China</h1>
+</div>
+
+
+<!--includes javascript at the bottom so the page loads faster-->
+<script type="text/javascript" charset="utf-8" src="js/furatto.min.js"></script>
+
+<!--loads analytics-->
+<script type="text/javascript">
+
+</script>
+
+<div class="modal-overlay"></div>
 </body>
 </html>
